569e7fa248d79da4bcb7721c5bc49346af796d81
Nikolay Vokhlachev <nvokhlachev@gmail.com>
2019-12-14 19:11:45 +0200

    This project added

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+export const AboutPage: React.FC = () => {
+  const history = useHistory()
+  return (
+    <>
+      <h1>Info page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nostrum
+        vero, repudiandae expedita beatae iure? Mollitia tempore quae qui
+        voluptatum.
+      </p>
+      <button className="btn" onClick={() => history.push('/')}>
+        Back to to-do list
+      </button>
+    </>
+  )
+}
